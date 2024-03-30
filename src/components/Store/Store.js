a8257f9ecdf6440944de34ab261944cc1616d66a
sirAvent <chiu7a@gmail.com>
2024-03-29 21:28:29 -0700

    Lift StoreView State

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -4,6 +4,8 @@ import { BsCaretLeftFill, BsCaretRightFill } from "react-icons/bs";
 import './store.css';
 import CategoryForm from './CategoryForm';
 import Product from './Product';
+import StoreView from './StoreView';
+
 
 
 export default function Store() {
@@ -110,46 +112,17 @@ export default function Store() {
         handleCategorySubmit={handleCategorySubmit}
       />
 
-      <div className='p-5 sm:p-0'>
-        <div className='flex flex-col sm:flex-row justify-between lg:pr-32 sm:mr-20 sm:mb-5 sm:gap-y-auto gap-y-3 gap-x-20 items-center'>
-          <span>
-            Results for
-          </span>
-          <div className='flex flex-row gap-x-3 items-center sm:mb-0 mb-5'>
-            < BsCaretLeftFill className='hover:cursor-pointer' onClick={handlePreviousPage} />
-            <form onSubmit={handlePageSubmit}>
-              <input
-                className='
-                h-[35px] w-[35px] outline-0 border-solid border-2 border-gray-900 rounded font-lg text-center black placeholder:text-black focus:placeholder:text-white
-                [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none
-                '
-                placeholder={page}
-                value={inputPage}
-                type='number'
-                onChange={handleInputPageChange}
-              />
-            </form>
-            
-              <span>...</span>
-              <span className=''>{maxPage}</span>
-            < BsCaretRightFill className='hover:cursor-pointer'  onClick={handleNextPage} />
-          </div>
-          
-        </div>
-        <div className='flex flex-row gap-y-20 sm:gap-10 flex-wrap justify-center sm:justify-even lg:pr-32'>
-          {isProductLoaded && products.map((product, index) => (
-            <Product
-              key = {index}
-              id = {product.id}
-              title={product.title}
-              price={product.price}
-              rating={product.rating}
-              images={product.images}
-            />
-          ))}
-        </div>
-        
-      </div>
+      <StoreView
+        products={products}
+        isProductLoaded={isProductLoaded}
+        page={page}
+        inputPage={inputPage}
+        maxPage={maxPage}
+        handlePreviousPage={handlePreviousPage}
+        handleNextPage={handleNextPage}
+        handlePageSubmit={handlePageSubmit}
+        handleInputPageChange={handleInputPageChange}
+      />
     </div>
   );
 }
---
+++ src/components/Store/StoreView.js
@@ -0,0 +1,56 @@
+import { BsCaretLeftFill, BsCaretRightFill } from "react-icons/bs";
+import Product from './Product';
+
+export default function StoreView({
+  products,
+  isProductLoaded,
+  page,
+  inputPage,
+  maxPage,
+  handlePreviousPage,
+  handleNextPage,
+  handlePageSubmit,
+  handleInputPageChange
+}) {
+  return (
+    <div className='p-5 sm:p-0'>
+      <div className='flex flex-col sm:flex-row justify-between lg:pr-32 sm:mr-20 sm:mb-5 sm:gap-y-auto gap-y-3 gap-x-20 items-center'>
+        <span>
+          Results for
+        </span>
+        <div className='flex flex-row gap-x-3 items-center sm:mb-0 mb-5'>
+          < BsCaretLeftFill className='hover:cursor-pointer' onClick={handlePreviousPage} />
+          <form onSubmit={handlePageSubmit}>
+            <input
+              className='
+              h-[35px] w-[35px] outline-0 border-solid border-2 border-gray-900 rounded font-lg text-center black placeholder:text-black focus:placeholder:text-white
+              [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none
+              '
+              placeholder={page}
+              value={inputPage}
+              type='number'
+              onChange={handleInputPageChange}
+            />
+          </form>
+          
+            <span>...</span>
+            <span className=''>{maxPage}</span>
+          < BsCaretRightFill className='hover:cursor-pointer'  onClick={handleNextPage} />
+        </div>
+        
+      </div>
+      <div className='flex flex-row gap-y-20 sm:gap-10 flex-wrap justify-center sm:justify-even lg:pr-32'>
+        {isProductLoaded && products.map((product, index) => (
+          <Product
+            key = {index}
+            id = {product.id}
+            title={product.title}
+            price={product.price}
+            rating={product.rating}
+            images={product.images}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
